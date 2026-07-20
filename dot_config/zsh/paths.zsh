39bc30f175b5f357dd85e42d17548852791325ef
Emil Zeinalov <105612843+mlznlv@users.noreply.github.com>
2026-07-20 21:49:36 +0100

    Detect optional executable paths by capability

--- dot_config/zsh/paths.zsh
+++ dot_config/zsh/paths.zsh
@@ -1,3 +1,7 @@
 typeset -U path PATH
 
+# User-local executables.
 [[ -d "$HOME/.local/bin" ]] && path+=("$HOME/.local/bin")
+
+# Optional local Docker Desktop/CLI installation.
+[[ -d "$HOME/.docker/bin" ]] && path+=("$HOME/.docker/bin")
