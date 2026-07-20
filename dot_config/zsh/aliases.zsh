5729715bfa22f4147991cef5be412acf4008fe82
mlznlv <105612843+mlznlv@users.noreply.github.com>
2026-07-20 17:03:52 +0100

    Add portable Git aliases

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,12 @@
+# Git
+alias g='git'
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
+alias gc='git commit'
+alias gca='git commit --amend'
+alias gco='git checkout'
+alias gd='git diff'
+alias gl='git pull'
+alias gp='git push'
+alias gst='git status'
