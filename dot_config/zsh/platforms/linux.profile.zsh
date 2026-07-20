2a4df71998a2ea324938c08ea66148b257b4b105
mlznlv <105612843+mlznlv@users.noreply.github.com>
2026-07-20 18:16:23 +0100

    Separate platform-specific login shell configuration

---
+++ dot_config/zsh/platforms/linux.profile.zsh
@@ -0,0 +1,1 @@
+# Linux login-shell configuration
---
+++ dot_config/zsh/platforms/macos.profile.zsh
@@ -0,0 +1,8 @@
+# macOS login-shell configuration
+
+# Homebrew
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -x /usr/local/bin/brew ]]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
